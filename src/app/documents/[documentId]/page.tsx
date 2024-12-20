e18c15dc753419974354837fd611955461d9264d
Jorge Matos <jisapjisap@gmail.com>
2024-12-20 16:11:05 +0100

    editor

---
+++ src/app/documents/[documentId]/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Editor } from './editor'
+
+
+interface DocumentIdPageProps {
+  params: Promise<{documentId: string}> 
+}
+
+const documentIdPage = async({params}: DocumentIdPageProps) => {
+  
+  const { documentId } = await params
+  
+  
+  return (
+    <div className='min-h-screen bg-[#FAFBFD]'>
+      <Editor />
+    </div>
+  )
+}
+
+export default documentIdPage
