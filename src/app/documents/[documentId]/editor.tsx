20ed2d55ec051b26ca6d11441d952917936deef0
Jorge Matos <jisapjisap@gmail.com>
2024-12-30 17:20:16 +0100

    Ruler

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -20,6 +20,7 @@ import Link from '@tiptap/extension-link'
 import TextAlign from '@tiptap/extension-text-align'
 import { FontSizeExtension } from '@/extensions/font-size'
 import { LineHeightExtension } from '@/extensions/line-height'
+import { Ruler } from './ruler'
 
 
 export const Editor = () => {
@@ -27,6 +28,9 @@ export const Editor = () => {
   const { setEditor } = useEditorStore();
 
   const editor = useEditor({
+
+    immediatelyRender: false,
+    
     onCreate({ editor }){
       setEditor(editor)
     },
@@ -106,12 +110,12 @@ export const Editor = () => {
         </tbody>
       </table>
       `,
-    immediatelyRender: false,
   })
 
 
   return (
     <div className='size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible'>
+      <Ruler />
       <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
         <EditorContent editor={editor} />
       </div>
---
+++ src/app/documents/[documentId]/ruler.tsx
@@ -0,0 +1,34 @@
+
+const markers = Array.from({ length: 83 }, (_, i) => i)
+
+export const Ruler = () => {
+  return(
+    <div className="h-6 border-b border-gray-300 flex items-end relative select-none print:hidden">
+      <div
+        id="ruler-container"
+        className="max-w-[816px] mx-auto w-full h-full relative"
+      >
+        <div className="absolute inset-x-0 bottom-0 h-full">
+          <div className="relative h-full w-[816px]"> 
+            {markers.map((marker) => {
+              const position = (marker * 816) /82;
+              return (
+                <div
+                  key={marker}
+                  className="absolute bottom-0"
+                  style={{ left: `${position}px`}}
+                >
+                  {marker % 10 === 0 && (
+                    <>
+                      <div className="absolute bottom-0 w-[1px] h-2 bg-neutral-500" />
+                    </>
+                  )}
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
