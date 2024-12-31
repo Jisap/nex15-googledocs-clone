185ce81fc6a553c14a27178ca99918e39f3cda91
Jorge Matos <jisapjisap@gmail.com>
2024-12-31 15:28:39 +0100

    Navbar

---
+++ src/app/documents/[documentId]/navbar.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image"
+
+
+
+
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Image 
+        src="/logo.svg" 
+        alt="Logo"
+      />
+    </nav>
+  )
+}
+
+export default Navbar
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Editor } from './editor'
 import { Toolbar } from './toolbar'
+import Navbar from './navbar'
 
 
 interface DocumentIdPageProps {
@@ -14,6 +15,7 @@ const documentIdPage = async({params}: DocumentIdPageProps) => {
   
   return (
     <div className='min-h-screen bg-[#FAFBFD]'>
+      <Navbar />
       <Toolbar />
       <Editor />
     </div>
