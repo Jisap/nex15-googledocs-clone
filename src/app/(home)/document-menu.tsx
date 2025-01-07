698ce37df99b8ff71498618fddee89528d16ecd9
Jorge Matos <jisapjisap@gmail.com>
2025-01-07 18:47:07 +0100

    feat: enhance DocumentMenu with dropdown for opening documents in a new tab

--- src/app/(home)/document-menu.tsx
+++ src/app/(home)/document-menu.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button"
-import { MoreVertical } from "lucide-react"
+import { ExternalLinkIcon, MoreVertical } from "lucide-react"
 import { Id } from "../../../convex/_generated/dataModel"
 import {
   DropdownMenu,
@@ -19,12 +19,24 @@ interface DocumentMenuProps {
 
 export const DocumentMenu = ({ documentId, title, onNewTab }: DocumentMenuProps) => {
   return (
-    <Button
-      variant="ghost"
-      size="icon"
-      className="rounded-full"
-    >
-      <MoreVertical className="size-4" />
-    </Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="rounded-full"
+        >
+          <MoreVertical className="size-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem
+          onClick={() => onNewTab(documentId)}
+        >
+          <ExternalLinkIcon className="size-4 mr-2"/>
+          Open in a new tab
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
