2a8b345f867ee3df33c2920058696ef043ff601c
Jorge Matos <jisapjisap@gmail.com>
2025-01-11 17:51:12 +0100

    feat: implement threaded comments and enhance editor with responsive design

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -22,6 +22,7 @@ import { FontSizeExtension } from '@/extensions/font-size'
 import { LineHeightExtension } from '@/extensions/line-height'
 import { Ruler } from './ruler'
 import { useLiveblocksExtension } from "@liveblocks/react-tiptap";
+import { Threads } from './threads'
 
 
 export const Editor = () => {
@@ -66,7 +67,9 @@ export const Editor = () => {
     },
     extensions: [
       liveBlocks,
-      StarterKit,
+      StarterKit.configure({
+        history: false,
+      }),
       LineHeightExtension.configure({
         types: ['heading', 'paragraph'],
         defaultLineHeight: 'normal',
@@ -106,6 +109,7 @@ export const Editor = () => {
       <Ruler />
       <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
         <EditorContent editor={editor} />
+        <Threads editor={editor} />
       </div>
     </div>
   )
