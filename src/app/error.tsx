f20dbc5056acf92e8ca2e954d4bfc093d5915a8d
Jorge Matos <jisapjisap@gmail.com>
2025-01-12 18:03:11 +0100

    feat: add ErrorPage component for improved error handling

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,8 +1,16 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
 import { AlertTriangleIcon } from "lucide-react"
+import Link from "next/link"
 
-const ErrorPage = () => {
+const ErrorPage = ({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center space-y-6">
       <div className="text-center space-y-4">
@@ -11,6 +19,28 @@ const ErrorPage = () => {
             <AlertTriangleIcon className="size-10 text-rose-600"/>
           </div>
         </div>
+        <div className="space-y-2">
+          <h2 className="text-xl font-semibold text-gray-900">
+            Some thing went wrong
+          </h2>
+        </div>
+      </div>
+      <div className="flex items-center gap-x-3">
+        <Button
+          onClick={reset}
+          className="font-medium px-6"
+        >
+          Try again
+        </Button>
+        <Button
+          asChild
+          variant="ghost"
+          className="font-medium"
+        >
+          <Link href="/">
+            Go back
+          </Link>
+        </Button>
       </div>
     </div>
   )
