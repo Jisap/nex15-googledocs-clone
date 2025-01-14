461d77f895f8b50068ed5d5b95f361627d509533
Jorge Matos <jisapjisap@gmail.com>
2025-01-14 17:27:34 +0100

    feat: refactor Inbox component to improve structure and add vertical separator

--- src/app/documents/[documentId]/inbox.tsx
+++ src/app/documents/[documentId]/inbox.tsx
@@ -10,20 +10,23 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 
 
 
 export const Inbox = () => {
   return (
     <ClientSideSuspense fallback={
-      <Button
-        variant={"ghost"}
-        className="relative"
-        size="icon"
-        disabled
+      <>
+        <Button
+          variant={"ghost"}
+          className="relative"
+          size="icon"
+          disabled
       >
-        <BellIcon className="size-5" />
-      </Button>
+          <BellIcon className="size-5" />
+        </Button>
+      </>
     }>
       <InboxMenu />
     </ClientSideSuspense>
@@ -33,40 +36,43 @@ export const Inbox = () => {
 const InboxMenu = () => {
 
   const { inboxNotifications } = useInboxNotifications();
-console.log("inboxNotifications", inboxNotifications)
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant={"ghost"}
-          className="relative"
-          size="icon"
-        >
-          <BellIcon  className="size-5"/>
-          {inboxNotifications?.length > 0 && (
-            <span className="absolute -top-1 -right-1 size-4 rounded-full bg-sky-500 text-xs text-white flex items-center justify-center">
-              {inboxNotifications.length}
-            </span>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant={"ghost"}
+            className="relative"
+            size="icon"
+          >
+            <BellIcon  className="size-5"/>
+            {inboxNotifications?.length > 0 && (
+              <span className="absolute -top-1 -right-1 size-4 rounded-full bg-sky-500 text-xs text-white flex items-center justify-center">
+                {inboxNotifications.length}
+              </span>
+            )}
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-auto">
+          {inboxNotifications.length > 0 ? (
+            <InboxNotificationList>
+              {inboxNotifications.map((inboxNotification) => (
+                <InboxNotification  
+                  key={inboxNotification.id}
+                  inboxNotification={inboxNotification}
+                />
+              ))}
+            </InboxNotificationList>
+          ) : (
+            <div className="p-2 w-[400px] text-center text-sm text-muted-foreground">
+              No notifications
+            </div>
           )}
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-auto">
-        {inboxNotifications.length > 0 ? (
-          <InboxNotificationList>
-            {inboxNotifications.map((inboxNotification) => (
-              <InboxNotification  
-                key={inboxNotification.id}
-                inboxNotification={inboxNotification}
-              />
-            ))}
-          </InboxNotificationList>
-        ) : (
-          <div className="p-2 w-[400px] text-center text-sm text-muted-foreground">
-            No notifications
-          </div>
-        )}
-      </DropdownMenuContent>
-    </DropdownMenu>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <Separator orientation="vertical" className="h-6"/>
+    </>
   )
 }
 
